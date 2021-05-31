5633728fb94d53e820247a1ea0cad2bc8621c154
rahulp7801 <76501505+rahulp7801@users.noreply.github.com>
2021-05-30 20:13:54 -0700

    Wikipedia and weather commands

--- cogs/misc.py
+++ cogs/misc.py
@@ -3,6 +3,8 @@ from discord.ext import commands
 import datetime
 import random
 import aiohttp
+import asyncio
+import wikipedia
 
 class Misc(commands.Cog):
     def __init__(self, client):
@@ -78,8 +80,9 @@ class Misc(commands.Cog):
                 print(data)
 
 
-            x = random.randrange(len(data['articles']))
+
             try:
+                x = random.randrange(len(data['articles']))
                 print(len(data['articles']))
                 likes = data['total_hits']
                 title = data['articles'][x]['title']
@@ -91,7 +94,7 @@ class Misc(commands.Cog):
                 thumbnail = data['articles'][x]['media']
                 desc = str(data['articles'][x]['summary'])[0:2000] + f"... \n[Click here to read the full story]({titleurl})"
                 embed = discord.Embed(title = title, description=desc, color = discord.Color.green())
-                embed.set_author(name = author)
+                embed.set_author(name = author, icon_url = 'https://th.bing.com/th/id/R3e0487abfc9c6b31806f4f6751fdc8ef?rik=hEriAUJIg5bKOA&riu=http%3a%2f%2fcdn.onlinewebfonts.com%2fsvg%2fimg_456930.png&ehk=bnskK1wWw7gco9UHxOQQxnCoBdDuCVGeyQsQTsaR2vk%3d&risl=&pid=ImgRaw')
                 embed.set_footer(text = f"👍🏽 {likes} | Published on {pubdate}")
                 embed.set_thumbnail(url=thumbnail)
                 await ctx.send(embed=embed)
@@ -100,6 +103,108 @@ class Misc(commands.Cog):
                                       f"If this keeps happening, contact the developers.")
 
 
+    @commands.command()
+    async def weather(self, ctx, *, place = 'New York'):
+        url = "https://community-open-weather-map.p.rapidapi.com/find"
+
+        querystring = {"q": place.strip(), "cnt": "1", "mode": "json", "lon": "0", "type": "link, accurate", "lat": "0",
+                       "units": "imperial"}
+
+        headers = {
+            'x-rapidapi-key': "f7bdac7e35msh94c0ae5a3b2d0c7p15b828jsnc6ac6c2d5034",
+            'x-rapidapi-host': "community-open-weather-map.p.rapidapi.com"
+        }
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url, headers=headers, params = querystring) as response:
+                data = await response.json()
+                print(data)
+        if int(data['count']) == 0:
+            return await ctx.send(f"It looks like the place `{place.strip()}` doesn't exist!")
+        author = data['list'][0]['name']
+        authorurl = 'https://th.bing.com/th/id/Rd8c10ced4c324b34408dda90292e48c2?rik=hLutIkyyLdbkMw&riu=http%3a%2f%2ficons.iconarchive.com%2ficons%2fiynque%2fios7-style%2f1024%2fWeather-icon.png&ehk=gA92r%2fgxkM24%2f0TYtSFkMie4BkV1Samb0ZYBjpJMqpQ%3d&risl=&pid=ImgRaw'
+        temp = data['list'][0]['main']['temp']
+        feelstemp = data['list'][0]['main']['feels_like']
+        tempmin = data['list'][0]['main']['temp_min']
+        tempmax = data['list'][0]['main']['temp_max']
+        pressure = data['list'][0]['main']['pressure']
+        humidity = data['list'][0]['main']['humidity']
+        print(humidity)
+        embed = discord.Embed(color = discord.Color.green())
+        descarr = []
+        if str(data['list'][0]['rain']) != 'None':
+            descarr.append('🌧️')
+        if int(data['list'][0]['wind']['speed']) > 20:
+            descarr.append('⛈')
+        if str(data['list'][0]['snow']) != 'None':
+            descarr.append('🌨️')
+        if int(data['list'][0]['clouds']['all']) > 60:
+            descarr.append('☁')
+        if len(descarr) == 0:
+            descarr.append(f'It\'s a nice day in {author} today!')
+        embed.set_author(name=f"Weather in {author}", icon_url=authorurl)
+        embed.set_footer(text = "All temperatures are in Fahrenheit.")
+        embed.add_field(name='Temperature', value=f"{temp}°F")
+        embed.add_field(name='Feels like', value = f"{feelstemp}°F")
+        embed.add_field(name='Minumum Temperature', value =f"{tempmin}°F", inline = False)
+        embed.add_field(name='Maximum Temperature', value = f"{tempmax}°F")
+        embed.add_field(name='Pressure', value = pressure, inline = False)
+        embed.add_field(name = 'Humidity', value = str(humidity))
+        desc = ' '.join(descarr)
+        print(desc)
+        embed.add_field(name='Weather', value=desc, inline = False)
+        await ctx.send(embed=embed)
+
+    @commands.command(aliases = ['wiki'])
+    async def wikipedia(self, ctx, *, q = "Elon Musk"):
+        while True:
+            try:
+                res = wikipedia.page(q.strip(), auto_suggest=False)
+                title = res.title
+                readmore = res.url
+                summ = str(wikipedia.summary(q.strip(), auto_suggest=False))[0:1800]
+                desc = summ + f"... \n[Continue Reading]({readmore})"
+                embed = discord.Embed(title=title, description=desc, color=discord.Color.green())
+                try:
+                    img = res.images[0]
+                    embed.set_thumbnail(url=img)
+                except IndexError:
+                    pass
+                await ctx.send(embed=embed)
+                return
+            except wikipedia.exceptions.PageError:
+                return await ctx.send(f"The search term `{q.strip()}` did not return a Wikipedia page.")
+            except wikipedia.exceptions.DisambiguationError as e:
+                arr = str(e).split('\n')
+                arr.pop(0)
+                descarr = []
+                for i, k in enumerate(arr):
+                    if i > 19:
+                        break
+                    descarr.append(f"{i + 1}. {k}")
+                desc = "\n".join(descarr)
+                await ctx.send(f"Which `{q.strip()}` did you mean? \n```{desc}```\nRespond with the number of the correct `{q.strip()}`.")
+                def check(m):
+                    return m.author == ctx.author and m.channel==ctx.channel
+
+                try:
+                    counter = 0
+                    while True:
+                        if counter > 5:
+                            return await ctx.send("Due to too many invalid choices, this session has ended.")
+                        msg = await self.client.wait_for('message', check=check, timeout = 120)
+                        try:
+                            q = arr.pop((int(msg.content) - 1))
+                            break
+                        except IndexError:
+                            await ctx.send(f"`{msg.content.lower()}` isn\'t a valid number in this list! Please try again!")
+                            counter += 1
+                            continue
+                        except ValueError:
+                            counter += 1
+                            await ctx.send(f"You need to specify the number, not `{msg.content.lower()}`.")
+                            continue
+                except asyncio.TimeoutError:
+                    return await ctx.reply("Timed out", mention_author = False)
 
 
 def setup(client):

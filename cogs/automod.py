a9cb118d1fb844630c0fd7621d2709d3ce8365f7
rahulp7801 <76501505+rahulp7801@users.noreply.github.com>
2021-07-07 17:00:41 -0700

    Added more automod commands, add custom punishments tomorrow

--- cogs/automod.py
+++ cogs/automod.py
@@ -22,6 +22,8 @@ cluster = MongoClient(mongo_url)
 
 profanity.load_censor_words()
 
+DISCORD_INVITE = r'discord(?:\.com|app\.com|\.gg)[\/invite\/]?(?:[a-zA-Z0-9\-]{2,32})'
+
 class BannedUser(Converter):
     async def convert(self, ctx, arg):
         if ctx.guild.me.guild_permissions.ban_members:
@@ -43,21 +45,36 @@ class Mod(Cog):
         self.url_regex = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
         self.links_allowed = []
         self.images_allowed = []
+        self.mass_mentions = []
+        self.all_caps = []
         self.addressed = {}
         self.profguilds = []
+        self.invitesblock = []
+        self.spoilers = []
         self.notready = True
 
     @commands.Cog.listener()
     async def on_ready(self):
         db = cluster["BLACKLIST"]
-        col = db['channels-links']
-        res = col.find()
+        col = db['channels']
+        res = col.find({"type":'link'})
         for i in res:
-            self.links_allowed.append(i["_id"])
-        col = db['channels-images']
-        res = col.find()
+            self.links_allowed.append(i["id"])
+        res = col.find({"type":'image'})
         for i in res:
-            self.images_allowed.append(i["_id"])
+            self.images_allowed.append(i["id"])
+        res = col.find({"type":'mention'})
+        for i in res:
+            self.mass_mentions.append(i["id"])
+        res = col.find({"type": 'caps'})
+        for i in res:
+            self.all_caps.append(i["id"])
+        res = col.find({"type": 'invite'})
+        for i in res:
+            self.invitesblock.append(i["id"])
+        res = col.find({"type": 'spoiler'})
+        for i in res:
+            self.spoilers.append(i["id"])
         db = cluster['CONFIGURATION']
         col = db['guilds']
         res = col.find()
@@ -76,8 +93,14 @@ class Mod(Cog):
                     and (datetime.utcnow() - m.created_at).seconds < 20
                     and m.channel == message.channel)
 
-        if not message.author.bot: #add action
-            print(message.channel.id)
+        if not message.author.bot:
+            try:
+                if len(message.mentions) > 7 and message.channel.id in self.mass_mentions: #only checks for humans i guess
+                    print('here sir')
+                    #warn/mute/whateveraction
+                    await message.reply("Warned for mass-mention.", delete_after = 20)
+            except Exception as e:
+                print(e)
             # if message.author.guild_permissions.manage_guild: return
             if len(list(filter(lambda m: _check(m), self.client.cached_messages))) >= 3:
                 try:
@@ -98,66 +121,78 @@ class Mod(Cog):
             if (message.channel.id in self.images_allowed and any([hasattr(a, "width") for a in message.attachments])):
                 await message.delete()
                 await message.channel.send("You can't send images here.", delete_after=10)
+            uppers = [l for l in message.clean_content if l.isupper() and l.isalpha()]
+            if (len(message.clean_content) == len(uppers) and message.channel.id in self.all_caps): #all caps
+                #custom action taken huh
+                await message.channel.send("bruh stop the caps")
+            if search(DISCORD_INVITE, message.content.lower()) and message.channel.id in self.invitesblock:
+                await message.channel.send("No invites", delete_after = 20)
+            if message.content.count('||') >= 6 and message.channel.id in self.spoilers:
+                await message.channel.send("Too many spoilers.", delete_after = 20)
 
     @commands.command()
     async def linktoggle(self, ctx, channel:Optional[discord.TextChannel], guild:Optional[bool]):
         if type(guild) == bool and not None:
             if guild:
                 db = cluster["BLACKLIST"]
-                col = db['channels-links']
+                col = db['channels']
                 for i in ctx.guild.text_channels:
-                    if col.count_documents({"_id": i.id}) == 0:
-                        col.insert_one({"_id": i.id})
+                    if col.count_documents({"id": i.id, "type":'link'}) == 0:
+                        col.insert_one({"id": i.id, "type":'link'})
                         self.links_allowed.append(i.id)
                 return await ctx.send("Success, added link blacklist to the server.")
             else:
                 db = cluster["BLACKLIST"]
-                col = db['channels-links']
+                col = db['channels']
                 for i in ctx.guild.text_channels:
-                    if col.count_documents({"_id": i.id}) != 0:
-                        col.delete_one({'_id':i.id})
+                    if col.count_documents({"id": i.id, "type":'link'}) != 0:
+                        col.delete_one({'id':i.id, "type":'link'})
                         self.links_allowed.remove(i.id)
                 return await ctx.send("Success, removed link blacklist for the server.")
         if channel is None:
             channel = ctx.channel
         db = cluster["BLACKLIST"]
-        col = db['channels-links']
-        if col.count_documents({"_id":channel.id}) == 0:
-            col.insert_one({"_id":channel.id})
+        col = db['channels']
+        if col.count_documents({"id":channel.id, "type":'link'}) == 0:
+            col.insert_one({"id":channel.id, "type":'link'})
             await ctx.send(f"Success, added {channel.mention} to Link-detection")
+            self.links_allowed.append(channel.id)
         else:
-            col.delete_one({"_id": channel.id})
+            col.delete_one({"id": channel.id, "type":'link'})
             await ctx.send(f"Success, removed {channel.mention} from Link-detection")
+            self.links_allowed.remove(channel.id)
 
     @commands.command()
     async def imagetoggle(self, ctx, channel: Optional[discord.TextChannel], guild: Optional[bool]):
         if type(guild) == bool and not None:
             if guild:
                 db = cluster["BLACKLIST"]
-                col = db['channels-images']
+                col = db['channels']
                 for i in ctx.guild.text_channels:
-                    if col.count_documents({"_id": i.id}) == 0:
-                        col.insert_one({"_id": i.id})
-                        self.links_allowed.append(i.id)
+                    if col.count_documents({"id": i.id, "type":'image'}) == 0:
+                        col.insert_one({"id": i.id, "type":'image'})
+                        self.images_allowed.append(i.id)
                 return await ctx.send("Success, added image blacklist to the server.")
             else:
                 db = cluster["BLACKLIST"]
-                col = db['channels-links']
+                col = db['channels']
                 for i in ctx.guild.text_channels:
-                    if col.count_documents({"_id": i.id}) != 0:
-                        col.delete_one({'_id': i.id})
-                        self.links_allowed.remove(i.id)
+                    if col.count_documents({"id": i.id, "type":'image'}) != 0:
+                        col.delete_one({'id': i.id, "type":'image'})
+                        self.images_allowed.remove(i.id)
                 return await ctx.send("Success, removed image blacklist for the server.")
         if channel is None:
             channel = ctx.channel
         db = cluster["BLACKLIST"]
-        col = db['channels-images']
-        if col.count_documents({"_id": channel.id}) == 0:
-            col.insert_one({"_id": channel.id})
+        col = db['channels']
+        if col.count_documents({"id": channel.id, "type":'image'}) == 0:
+            col.insert_one({"id": channel.id, "type":'image'})
             await ctx.send(f"Success, added {channel.mention} to Image-detection")
+            self.images_allowed.remove(channel.id)
         else:
-            col.delete_one({"_id": channel.id})
+            col.delete_one({"id": channel.id, "type":'image'})
             await ctx.send(f"Success, removed {channel.mention} from Image-detection")
+            self.images_allowed.remove(channel.id)
 
     @commands.command(aliases=['bl', 'blackl'], help='Toggle the blacklisted word detection!')
     @commands.has_permissions(manage_guild=True)
@@ -190,6 +225,133 @@ class Mod(Cog):
             await ctx.reply(f'**{ctx.author.name}**, you don\'t have permission.', mention_author=False)
             return
 
+    @commands.command()
+    async def mentiontoggle(self, ctx, channel: Optional[discord.TextChannel], guild: Optional[bool]):
+        if type(guild) == bool and not None:
+            if guild:
+                db = cluster["BLACKLIST"]
+                col = db['channels']
+                for i in ctx.guild.text_channels:
+                    if col.count_documents({"id": i.id, "type": 'mention'}) == 0:
+                        col.insert_one({"id": i.id, "type": 'mention'})
+                        self.mass_mentions.append(i.id)
+                return await ctx.send("Success, added mass-mention blacklist to the server.")
+            else:
+                db = cluster["BLACKLIST"]
+                col = db['channels']
+                for i in ctx.guild.text_channels:
+                    if col.count_documents({"id": i.id, "type": 'mention'}) != 0:
+                        col.delete_one({'id': i.id, "type": 'mention'})
+                        self.mass_mentions.remove(i.id)
+                return await ctx.send("Success, removed mass-mention blacklist for the server.")
+        if channel is None:
+            channel = ctx.channel
+        db = cluster["BLACKLIST"]
+        col = db['channels']
+        if col.count_documents({"id": channel.id, "type": 'mention'}) == 0:
+            col.insert_one({"id": channel.id, "type": 'mention'})
+            await ctx.send(f"Success, added {channel.mention} to mass-mention blacklist.")
+            self.mass_mentions.append(channel.id)
+        else:
+            col.delete_one({"id": channel.id, "type": 'mention'})
+            await ctx.send(f"Success, removed {channel.mention} from mass-mention blacklist.")
+            self.mass_mentions.remove(channel.id)
+
+    @commands.command()
+    async def capslock(self, ctx, channel: Optional[discord.TextChannel], guild: Optional[bool]):
+        if type(guild) == bool and not None:
+            if guild:
+                db = cluster["BLACKLIST"]
+                col = db['channels']
+                for i in ctx.guild.text_channels:
+                    if col.count_documents({"id": i.id, "type": 'caps'}) == 0:
+                        col.insert_one({"id": i.id, "type": 'caps'})
+                        self.all_caps.append(i.id)
+                return await ctx.send("Success, added all-caps blacklist to the server.")
+            else:
+                db = cluster["BLACKLIST"]
+                col = db['channels']
+                for i in ctx.guild.text_channels:
+                    if col.count_documents({"id": i.id, "type": 'caps'}) != 0:
+                        col.delete_one({'id': i.id, "type": 'caps'})
+                        self.all_caps.remove(i.id)
+                return await ctx.send("Success, removed all-caps blacklist for the server.")
+        if channel is None:
+            channel = ctx.channel
+        db = cluster["BLACKLIST"]
+        col = db['channels']
+        if col.count_documents({"id": channel.id, "type": 'caps'}) == 0:
+            col.insert_one({"id": channel.id, "type": 'caps'})
+            await ctx.send(f"Success, added {channel.mention} to all-caps detection")
+            self.all_caps.append(channel.id)
+        else:
+            col.delete_one({"id": channel.id, "type": 'caps'})
+            await ctx.send(f"Success, removed {channel.mention} from all-caps detection")
+            self.all_caps.remove(channel.id)
+
+    @commands.command()
+    async def inviteblock(self, ctx, channel: Optional[discord.TextChannel], guild: Optional[bool]):
+        if type(guild) == bool and not None:
+            if guild:
+                db = cluster["BLACKLIST"]
+                col = db['channels']
+                for i in ctx.guild.text_channels:
+                    if col.count_documents({"id": i.id, "type": 'invite'}) == 0:
+                        col.insert_one({"id": i.id, "type": 'invite'})
+                        self.invitesblock.append(i.id)
+                return await ctx.send("Success, added invite blacklist to the server.")
+            else:
+                db = cluster["BLACKLIST"]
+                col = db['channels']
+                for i in ctx.guild.text_channels:
+                    if col.count_documents({"id": i.id, "type": 'invite'}) != 0:
+                        col.delete_one({'id': i.id, "type": 'invite'})
+                        self.invitesblock.remove(i.id)
+                return await ctx.send("Success, removed invite blacklist for the server.")
+        if channel is None:
+            channel = ctx.channel
+        db = cluster["BLACKLIST"]
+        col = db['channels']
+        if col.count_documents({"id": channel.id, "type": 'invite'}) == 0:
+            col.insert_one({"id": channel.id, "type": 'invite'})
+            await ctx.send(f"Success, added {channel.mention} to invite detection")
+            self.invitesblock.append(channel.id)
+        else:
+            col.delete_one({"id": channel.id, "type": 'invite'})
+            await ctx.send(f"Success, removed {channel.mention} from invite detection")
+            self.invitesblock.remove(channel.id)
+
+    @commands.command()
+    async def spoilertoggle(self, ctx, channel: Optional[discord.TextChannel], guild: Optional[bool]):
+        if type(guild) == bool and not None:
+            if guild:
+                db = cluster["BLACKLIST"]
+                col = db['channels']
+                for i in ctx.guild.text_channels:
+                    if col.count_documents({"id": i.id, "type": 'spoiler'}) == 0:
+                        col.insert_one({"id": i.id, "type": 'spoiler'})
+                        self.spoilers.append(i.id)
+                return await ctx.send("Success, added spoiler blacklist to the server.")
+            else:
+                db = cluster["BLACKLIST"]
+                col = db['channels']
+                for i in ctx.guild.text_channels:
+                    if col.count_documents({"id": i.id, "type": 'spoiler'}) != 0:
+                        col.delete_one({'id': i.id, "type": 'spoiler'})
+                        self.spoilers.remove(i.id)
+                return await ctx.send("Success, removed spoiler blacklist for the server.")
+        if channel is None:
+            channel = ctx.channel
+        db = cluster["BLACKLIST"]
+        col = db['channels']
+        if col.count_documents({"id": channel.id, "type": 'spoiler'}) == 0:
+            col.insert_one({"id": channel.id, "type": 'spoiler'})
+            await ctx.send(f"Success, added {channel.mention} to spoiler-detection")
+            self.spoilers.append(channel.id)
+        else:
+            col.delete_one({"id": channel.id, "type": 'spoiler'})
+            await ctx.send(f"Success, removed {channel.mention} from spoiler-detection")
+            self.spoilers.remove(channel.id)
 
 def setup(client):
     client.add_cog(Mod(client))
